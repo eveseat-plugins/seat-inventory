8d6f6239110b9ab855269568346bb79455bb4d57
recursivetree <recursivetreemail@gmail.com>
2025-01-27 19:50:26 +0100

    finish reverting Gelope's contributions

--- src/resources/lang/fr/settings.php
+++ src/resources/lang/fr/settings.php
@@ -1,49 +1,6 @@
 <?php
 
 return [
-    'settings_title' => 'Paramètres',
-
-    'workspace_settings_title' => 'Workspace Settings',
-
-    'workspace_name_field' => 'Name',
-    'workspace_name_placeholder' => 'Enter the workspace\'s name...',
-    'delete_workspace_btn' => 'Delete Workspace',
-
     'notifications_label' => 'Notifications',
-
     'alliances_title' => 'Alliances',
-    'add_alliance_btn' => 'Ajouter des Alliances',
-    'select_alliance_placeholder' => 'Sélectionnez une alliance',
-    'add_alliance_success' => 'Ajouté avec succès',
-
-    'add_members_btn' => 'Add Members',
-    'add_members_success' => 'Successfully added members of',
-    'add_members_tooltip' => 'Existing and new alliance members will be added automatically.',
-
-    'remove_members_btn' => 'Supprimer des membres',
-    'remove_members_success' => 'Suppression réussie des membres de',
-    'remove_members_tooltip' => 'Les corporations ajoutées automatiquement seront supprimées et aucune nouvelle corporation ne sera ajoutée à l\'avenir. Les corporations ajoutées manuellement resteront.',
-
-    'settings_save_success' => 'Les paramètres ont été modifiés avec succès',
-    'workspace_delete_success' => 'Espace de travail supprimé avec succès!',
-
-    'corporations_title' => 'Corporations',
-    'add_corporations_btn' => 'Ajouter corporations',
-    'select_corporation_placeholder' => 'Choisir une corporation',
-    'add_corporation_success' => 'Ajouté avec succès',
-
-    'markets_title' => 'Marchés',
-    'add_market_btn' => 'Ajouter un marché',
-    'add_market_tooltip' => 'Votre personnage principal sera utilisé pour extraire des données d’ESI. Veuillez vous assurer que votre personnage principal a accès au marché sur cette structure. Actuellement seules les citadelles fonctionnent.',
-    'remove_market_btn' => 'Supprimer le marché',
-
-    'remove_market_success' => 'Suppression réussie des membres de',
-
-    'error_save_settings' => 'Failed to change the settings',
-    'error_delete_workspace' => 'Failed to deleted workspace!',
-    'error_adding_alliance' => 'Failed to add',
-    'error_adding_members' => 'Failed to add members of',
-    'error_remove_members' => 'Failed to add members of',
-    'error_adding_corporations' => 'Failed to add',
-    'error_removing_markets' => 'Failed to add members of',
 ];
