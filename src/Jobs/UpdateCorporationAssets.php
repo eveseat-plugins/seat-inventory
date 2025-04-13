2b64d48688f8d819acdb5d80de6827eda0138269
recursivetree <recursivetreemail@gmail.com>
2025-04-13 19:44:03 +0200

    fix missing import

--- src/Jobs/UpdateCorporationAssets.php
+++ src/Jobs/UpdateCorporationAssets.php
@@ -2,6 +2,7 @@
 
 namespace RecursiveTree\Seat\Inventory\Jobs;
 
+use DateTime;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
